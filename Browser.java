2c2b1bd77047c191770ef556544e47216ddcd585
Zafir Hasan <zafirhsn@gmail.com>
2015-01-08 09:25:38 -0500

    Browser

--- Browser.java
+++ Browser.java
@@ -21,27 +21,28 @@ public class Browser {
 
     public static boolean search() {
 	for (int i=0;i<wordlist.size();i++) {
+	    if (wordlist.get(i).substring(0,1).equals("<")) {
 
-	    if (wordlist.get(i).equals("<html>")) {
-		// call html function
+		if (wordlist.get(i).equals("<html>")) {
+		    // call html function
+		}
+		if (wordlist.get(i).equals("<title>")) {
+		    // call title function
+		}
+		if (wordlist.get(i).equals("<header>")) {
+		    // call header function
+		}
+		if (wordlist.get(i).equals("<body>")) {
+		    // call body function
+		}
+		if (wordlist.get(i).equals("<b>")) {
+		    // call bod function
+		}
+		if (wordlist.get(i).equals("<i>")) {
+		}
+		if (wordlist.get(i).equals("<u>")) {
+		}
 	    }
-	    if (wordlist.get(i).equals("<title>")) {
-		// call title function
-	    }
-	    if (wordlist.get(i).equals("<header>")) {
-		// call header function
-	    }
-	    if (wordlist.get(i).equals("<body>")) {
-		// call body function
-	    }
-	    if (wordlist.get(i).equals("<b>")) {
-		// call bod function
-	    }
-	    if (wordlist.get(i).equals("<i>")) {
-	    }
-	    if (wordlist.get(i).equals("<u>")) {
-	    }
-	    
 	    
 	}
 	return false;
